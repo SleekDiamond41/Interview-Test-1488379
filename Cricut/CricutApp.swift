f3561176b1444d2d79808cbddf4bab2e372e61ba
Michael Arrington <29713320+SleekDiamond41@users.noreply.github.com>
2025-03-03 18:15:54 -0700

    Initial Commit

---
+++ Cricut/CricutApp.swift
@@ -0,0 +1,17 @@
+//
+//  CricutApp.swift
+//  Cricut
+//
+//  Created by Michael Arrington on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct CricutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
